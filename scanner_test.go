240984d1365c849604525749019dfb8e878c155b
ihcsim <ihcsim@gmail.com>
2017-08-23 14:07:49 -0700

    New unit tests for scanner that reads from a redis mock

--- scanner_test.go
+++ scanner_test.go
@@ -2,84 +2,182 @@ package zapit_test
 
 import (
 	"testing"
+	"time"
 
+	"github.com/alicebob/miniredis"
 	"github.com/ihcsim/zapit"
 	"github.com/ihcsim/zapit/internal/db"
 )
 
 func TestIsSafe(t *testing.T) {
-	dbStore := &db.InMemoryDB{}
-	scanner := zapit.NewScanner(dbStore)
+	t.Run("In Memory", func(t *testing.T) {
+		dbStore := &db.InMemoryDB{}
+		scanner := zapit.NewScanner(dbStore)
 
-	t.Run("Bad domains", func(t *testing.T) {
-		testCases := []struct {
-			url      string
-			result   string
-			expected bool
-		}{
-			{url: "google.com", result: "safe", expected: true},
-			{url: "localhost:80", result: "safe", expected: true},
-			{url: "localhost:8080/test", result: "safe", expected: true},
-			{url: "127.0.0.1", result: "safe", expected: true},
-			{url: "dont.exist.com", result: "safe", expected: true},
-			{url: "linksk.us", result: "unsafe", expected: false},
-			{url: "linksk.us:8080", result: "unsafe", expected: false},
-			{url: "linksk.us:8080/accounts", result: "unsafe", expected: false},
-			{url: "linksk.us:8080/accounts?foo=bar", result: "unsafe", expected: false},
-			{url: "linksk.us:8080/accounts?foo=bar#anchor", result: "unsafe", expected: false},
-			{url: "linksk.us/accounts", result: "unsafe", expected: false},
-			{url: "linksk.us/accounts?foo=bar", result: "unsafe", expected: false},
-			{url: "linksk.us/accounts?foo=bar#anchor", result: "unsafe", expected: false},
-			{url: "linksk.us/accounts#anchor", result: "unsafe", expected: false},
-			{url: "linksk.us?foo=bar", result: "unsafe", expected: false},
-			{url: "linksk.us#support", result: "unsafe", expected: false},
-			{url: "108.61.210.89", result: "unsafe", expected: false},
-			{url: "108.61.210.89:8080", result: "unsafe", expected: false},
-			{url: "108.61.210.89:8080/transactions", result: "unsafe", expected: false},
-			{url: "108.61.210.89:8080/transactions?foo=bar", result: "unsafe", expected: false},
-			{url: "108.61.210.89:8080/transactions?foo=bar#anchor", result: "unsafe", expected: false},
-			{url: "108.61.210.89/transaction", result: "unsafe", expected: false},
-			{url: "108.61.210.89/transaction?foo=bar", result: "unsafe", expected: false},
-			{url: "108.61.210.89/transaction?foo=bar#anchor", result: "unsafe", expected: false},
-			{url: "108.61.210.89/transaction#anchor", result: "unsafe", expected: false},
-			{url: "108.61.210.89?foo=bar", result: "unsafe", expected: false},
-			{url: "108.61.210.89#anchor", result: "unsafe", expected: false},
-			{url: "app.linksk.us", result: "unsafe", expected: false},
-			{url: "blog.linksk.us", result: "unsafe", expected: false},
-			{url: "user.support.linksk.us", result: "unsafe", expected: false},
-			//{url: "docs.google.com?user=rogue&worm=jimbo", result: "unsafe", expected: false},
-		}
-
-		for _, testCase := range testCases {
-			actual, err := scanner.IsSafe(testCase.url)
-			if err != nil {
-				t.Fatal("Unexpected error: ", err)
+		t.Run("Bad domains", func(t *testing.T) {
+			testCases := []struct {
+				url      string
+				result   string
+				expected bool
+			}{
+				{url: "google.com", result: "safe", expected: true},
+				{url: "localhost:80", result: "safe", expected: true},
+				{url: "localhost:8080/test", result: "safe", expected: true},
+				{url: "127.0.0.1", result: "safe", expected: true},
+				{url: "dont.exist.com", result: "safe", expected: true},
+				{url: "linksk.us", result: "unsafe", expected: false},
+				{url: "linksk.us:8080", result: "unsafe", expected: false},
+				{url: "linksk.us:8080/accounts", result: "unsafe", expected: false},
+				{url: "linksk.us:8080/accounts?foo=bar", result: "unsafe", expected: false},
+				{url: "linksk.us:8080/accounts?foo=bar#anchor", result: "unsafe", expected: false},
+				{url: "linksk.us/accounts", result: "unsafe", expected: false},
+				{url: "linksk.us/accounts?foo=bar", result: "unsafe", expected: false},
+				{url: "linksk.us/accounts?foo=bar#anchor", result: "unsafe", expected: false},
+				{url: "linksk.us/accounts#anchor", result: "unsafe", expected: false},
+				{url: "linksk.us?foo=bar", result: "unsafe", expected: false},
+				{url: "linksk.us#support", result: "unsafe", expected: false},
+				{url: "108.61.210.89", result: "unsafe", expected: false},
+				{url: "108.61.210.89:8080", result: "unsafe", expected: false},
+				{url: "108.61.210.89:8080/transactions", result: "unsafe", expected: false},
+				{url: "108.61.210.89:8080/transactions?foo=bar", result: "unsafe", expected: false},
+				{url: "108.61.210.89:8080/transactions?foo=bar#anchor", result: "unsafe", expected: false},
+				{url: "108.61.210.89/transaction", result: "unsafe", expected: false},
+				{url: "108.61.210.89/transaction?foo=bar", result: "unsafe", expected: false},
+				{url: "108.61.210.89/transaction?foo=bar#anchor", result: "unsafe", expected: false},
+				{url: "108.61.210.89/transaction#anchor", result: "unsafe", expected: false},
+				{url: "108.61.210.89?foo=bar", result: "unsafe", expected: false},
+				{url: "108.61.210.89#anchor", result: "unsafe", expected: false},
+				{url: "app.linksk.us", result: "unsafe", expected: false},
+				{url: "blog.linksk.us", result: "unsafe", expected: false},
+				{url: "user.support.linksk.us", result: "unsafe", expected: false},
+				{url: "docs.google.com?user=rogue&worm=jimbo", result: "unsafe", expected: false},
 			}
 
-			if actual.IsSafe != testCase.expected {
-				t.Errorf("Expected URL %s to be %s", testCase.url, testCase.result)
+			for _, testCase := range testCases {
+				actual, err := scanner.IsSafe(testCase.url)
+				if err != nil {
+					t.Fatal("Unexpected error: ", err)
+				}
+
+				if actual.IsSafe != testCase.expected {
+					t.Errorf("Expected URL %s to be %s", testCase.url, testCase.result)
+				}
 			}
-		}
+		})
+
+		t.Run("Good domains with malicious content", func(t *testing.T) {
+			testCases := []struct {
+				url      string
+				result   string
+				expected bool
+			}{
+				{url: "docs.google.com/document/d/malicious_document_1", result: "unsafe", expected: false},
+			}
+
+			for _, testCase := range testCases {
+				actual, err := scanner.IsSafe(testCase.url)
+				if err != nil {
+					t.Fatal("Unexpected error: ", err)
+				}
+
+				if actual.IsSafe != testCase.expected {
+					t.Errorf("Expected URL %s to be %s", testCase.url, testCase.result)
+				}
+			}
+		})
 	})
 
-	t.Run("Good domains with malicious content", func(t *testing.T) {
-		testCases := []struct {
-			url      string
-			result   string
-			expected bool
-		}{
-			{url: "docs.google.com/document/d/malicious_document_1", result: "unsafe", expected: false},
+	t.Run("Redis", func(t *testing.T) {
+		redis, err := miniredis.Run()
+		if err != nil {
+			t.Fatal(err)
 		}
+		defer redis.Close()
 
-		for _, testCase := range testCases {
-			actual, err := scanner.IsSafe(testCase.url)
-			if err != nil {
-				t.Fatal("Unexpected error: ", err)
+		dbStore, err := db.NewRedis(redis.Addr(), "tcp", time.Millisecond*200)
+		scanner := zapit.NewScanner(dbStore)
+
+		t.Run("Bad domains", func(t *testing.T) {
+			testCases := []struct {
+				url      string
+				result   string
+				expected bool
+			}{
+				{url: "google.com", result: "safe", expected: true},
+				{url: "localhost:80", result: "safe", expected: true},
+				{url: "localhost:8080/test", result: "safe", expected: true},
+				{url: "127.0.0.1", result: "safe", expected: true},
+				{url: "dont.exist.com", result: "safe", expected: true},
+				{url: "linksk.us", result: "unsafe", expected: false},
+				{url: "linksk.us:8080", result: "unsafe", expected: false},
+				{url: "linksk.us:8080/accounts", result: "unsafe", expected: false},
+				{url: "linksk.us:8080/accounts?foo=bar", result: "unsafe", expected: false},
+				{url: "linksk.us:8080/accounts?foo=bar#anchor", result: "unsafe", expected: false},
+				{url: "linksk.us/accounts", result: "unsafe", expected: false},
+				{url: "linksk.us/accounts?foo=bar", result: "unsafe", expected: false},
+				{url: "linksk.us/accounts?foo=bar#anchor", result: "unsafe", expected: false},
+				{url: "linksk.us/accounts#anchor", result: "unsafe", expected: false},
+				{url: "linksk.us?foo=bar", result: "unsafe", expected: false},
+				{url: "linksk.us#support", result: "unsafe", expected: false},
+				{url: "108.61.210.89", result: "unsafe", expected: false},
+				{url: "108.61.210.89:8080", result: "unsafe", expected: false},
+				{url: "108.61.210.89:8080/transactions", result: "unsafe", expected: false},
+				{url: "108.61.210.89:8080/transactions?foo=bar", result: "unsafe", expected: false},
+				{url: "108.61.210.89:8080/transactions?foo=bar#anchor", result: "unsafe", expected: false},
+				{url: "108.61.210.89/transaction", result: "unsafe", expected: false},
+				{url: "108.61.210.89/transaction?foo=bar", result: "unsafe", expected: false},
+				{url: "108.61.210.89/transaction?foo=bar#anchor", result: "unsafe", expected: false},
+				{url: "108.61.210.89/transaction#anchor", result: "unsafe", expected: false},
+				{url: "108.61.210.89?foo=bar", result: "unsafe", expected: false},
+				{url: "108.61.210.89#anchor", result: "unsafe", expected: false},
+				{url: "app.linksk.us", result: "unsafe", expected: false},
+				{url: "blog.linksk.us", result: "unsafe", expected: false},
+				{url: "user.support.linksk.us", result: "unsafe", expected: false},
+				{url: "docs.google.com?user=rogue&worm=jimbo", result: "unsafe", expected: false},
 			}
 
-			if actual.IsSafe != testCase.expected {
-				t.Errorf("Expected URL %s to be %s", testCase.url, testCase.result)
+			for _, testCase := range testCases {
+				safe := testCase.expected
+				if !safe {
+					redis.Set(testCase.url, "")
+				}
+
+				actual, err := scanner.IsSafe(testCase.url)
+				if err != nil {
+					t.Fatal("Unexpected error: ", err)
+				}
+
+				if actual.IsSafe != testCase.expected {
+					t.Errorf("Expected URL %s to be %s", testCase.url, testCase.result)
+				}
 			}
-		}
+		})
+
+		t.Run("Good domains with malicious content", func(t *testing.T) {
+			testCases := []struct {
+				url      string
+				result   string
+				expected bool
+			}{
+				{url: "docs.google.com/document/d/malicious_document_1", result: "unsafe", expected: false},
+			}
+
+			for _, testCase := range testCases {
+				safe := testCase.expected
+				if !safe {
+					redis.Set(testCase.url, "")
+				}
+
+				actual, err := scanner.IsSafe(testCase.url)
+				if err != nil {
+					t.Fatal("Unexpected error: ", err)
+				}
+
+				if actual.IsSafe != testCase.expected {
+					t.Errorf("Expected URL %s to be %s", testCase.url, testCase.result)
+				}
+			}
+		})
+
 	})
 }
